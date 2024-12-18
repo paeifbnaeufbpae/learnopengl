2a322cf68222247916ef71b265a3620f135fa7d7
merlinshabaj <merlin.goessling@gmail.com>
2024-12-18 21:16:33 +0100

    Using indexed rendering for a rectangle

--- src/main.cc
+++ src/main.cc
@@ -62,9 +62,14 @@ int main() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     float vertices[] = {
-      -0.5f, -0.5f, 0.0f,
-       0.5f, -0.5f, 0.0f,
-       0.0f,  0.5f, 0.0f
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {
+      0, 1, 3,
+      1, 2, 3
     };
 
     unsigned int VBO;
@@ -105,19 +110,26 @@ int main() {
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
 
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
     // 1. bind Vertex Array Object
     glBindVertexArray(VAO);
     // 2. copy our vertices array in a buffer for OpenGL to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // 3. then set our vertex attributes pointers
+    // 3. copy our index array in a element buffer for OpenGL to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 4. then set our vertex attributes pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
 
     // check and call events and swap the buffers
     glfwSwapBuffers(window);
